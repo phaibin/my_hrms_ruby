db36b37181e50e5e8b52fac23f4a2840587e3baa
Leon <phaibin@gmail.com>
2012-10-06 14:46:43 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyHrmsRuby::Application.config.secret_token = '28ff87b685af58e4e58af07dbbc275ba42b2c08f070c30aa0b1ca78545604ad6f69193f85659fecb6b874ab3103435fce4a91a348fd6c656dc18fff5b763c588'
